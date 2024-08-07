9a591f42d6156dced2dbba2b18d40b5061e15067
k3v-5 <142853881+k3v-5@users.noreply.github.com>
2024-08-07 13:38:10 -0600

    ShowCryptos

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
 import './Navbar.css'
 import DiamondSharpIcon from '@mui/icons-material/DiamondSharp';
+import { useContext } from 'react';
+import { CoinContext } from '../../context/CoinContext';
 
 const Navbar = () => {
+
+  const {setCurrency} = useContext(CoinContext)
+
+  
+  const changeCurrency = (e) => {
+    switch(e.target.value)
+    {
+      case 'usd':
+        {
+        setCurrency({name:'usd',symbol:'$'});
+        break;
+      }
+      case 'eur':
+       {
+        setCurrency({name:'eur',symbol:'€'});
+        break;
+       }
+      case 'mxn':
+        {
+          setCurrency({name:'mxn',symbol:'$'});
+        break;
+        }
+      default:
+        {
+          setCurrency({name:'usd',symbol:'$'});
+        break;
+        }
+    }
+  }
   return (
     <div className='navbar'>
    <button className="logo" ><DiamondSharpIcon fontSize="large" /></button>
@@ -13,7 +44,7 @@ const Navbar = () => {
             <li>Contact</li>
    </ul>
         <div className="nav-right">
-            <select >
+            <select onChange={changeCurrency}>
                 <option value="usd">USD</option>
                 <option value="eur">EUR</option>
                 <option value="mxn">MXN</option>
